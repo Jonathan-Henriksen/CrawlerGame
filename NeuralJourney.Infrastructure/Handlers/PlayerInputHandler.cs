800340c711e1aa5f1eb5f82c6655db7cd88ad8f5
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2023-08-16 17:10:12 +0200

    yep yep

--- NeuralJourney.Infrastructure/Handlers/PlayerInputHandler.cs
+++ NeuralJourney.Infrastructure/Handlers/PlayerInputHandler.cs
@@ -1,16 +1,13 @@
-﻿using NeuralJourney.Core.Constants.Messages;
+﻿using NeuralJourney.Core.Exceptions;
 using NeuralJourney.Core.Interfaces.Handlers;
 using NeuralJourney.Core.Interfaces.Services;
 using NeuralJourney.Core.Models.World;
 using Serilog;
-using System.Net.Sockets;
 
 namespace NeuralJourney.Infrastructure.Handlers
 {
     public class PlayerInputHandler : IInputHandler<Player>
     {
-        private const int MaxReconnectionAttempts = 3;
-
         private readonly IMessageService _messageService;
         private readonly ILogger _logger;
 
@@ -27,14 +24,11 @@ namespace NeuralJourney.Infrastructure.Handlers
         {
             var client = player.GetClient();
 
-            var retryCount = 0;
             while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
                     var input = await _messageService.ReadMessageAsync(client, cancellationToken);
-
-                    retryCount = 0; // Reset after reading a message successfully
 
                     if (_messageService.IsCloseConnectionMessage(input))
                     {
@@ -46,40 +40,20 @@ namespace NeuralJourney.Infrastructure.Handlers
                         continue;
 
                     OnInputReceived?.Invoke(input, player);
-
-
-                }
-                catch (IOException ex)
-                {
-                    if (ex.InnerException is SocketException socketEx && socketEx.SocketErrorCode == SocketError.ConnectionReset)
-                    {
-                        _logger.Information(InfoMessageTemplates.ClientDisconnected, client.Client.RemoteEndPoint);
-                        return;
-                    }
-
-                    _logger.Warning(ex, "Lost connection to player");
-
-                    return;
                 }
                 catch (OperationCanceledException)
                 {
                     return;
                 }
+                catch (MessageException ex)
+                {
+                    await _messageService.SendMessageAsync(client, ex.Message, cancellationToken);
+                    await Task.Delay(500, cancellationToken);
+                }
                 catch (Exception ex)
                 {
-                    _logger.Error(ex, "Unexpected error while reading from player stream {Address}. Retries: {RetryCount}/3", client.Client.RemoteEndPoint, retryCount);
+                    _logger.Error(ex, "Unexpected error while handling player input");
 
-                    if (retryCount < 3)
-                    {
-                        retryCount++;
-
-                        if (client.Connected)
-                            await _messageService.SendMessageAsync(client, $"Unexpected error while processing your message. Please try again ({retryCount + 1}/3)", cancellationToken);
-
-                        continue;
-                    }
-
-                    await _messageService.SendMessageAsync(client, "Retry limit exceeded", cancellationToken);
                     await _messageService.SendCloseConnectionAsync(client, cancellationToken);
 
                     return;
